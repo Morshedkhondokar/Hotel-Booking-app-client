21cb3bdef4e039a9fafde22e9ef862cc60bf608b
Morshed Khondokar <morshedkhondokar99@.com>
2025-11-05 15:54:17 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+
+const App = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <Outlet/>
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default App
